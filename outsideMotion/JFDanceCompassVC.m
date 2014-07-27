6295954bbc50f4d094002923f3af04e0b2ba3861
Jared Fishman <jfish@gigbear.com>
2014-07-26 23:36:41 -0700

    very basic compass shape

---
+++ outsideMotion/JFDanceCompassVC.m
@@ -0,0 +1,89 @@
+//
+//  JFDanceCompassVC.m
+//  outsideMotion
+//
+//  Created by Jared Fishman on 7/26/14.
+//  Copyright (c) 2014 Jared Fishman. All rights reserved.
+//
+
+#import "JFDanceCompassVC.h"
+
+@interface JFDanceCompassVC ()
+
+@end
+
+@implementation JFDanceCompassVC
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    [self createTheCentralCircle];
+    
+    [self createTheCompassPointer];
+    
+    // Do any additional setup after loading the view.
+}
+
+-(void)createTheCentralCircle
+{
+    int radius = 100;
+    CAShapeLayer *circle = [CAShapeLayer layer];
+    // Make a circular shape
+    circle.path = [UIBezierPath bezierPathWithRoundedRect:CGRectMake(0, 0, 2.0*radius, 2.0*radius)
+                                             cornerRadius:radius].CGPath;
+    // Center the shape in self.view
+    circle.position = CGPointMake(CGRectGetMidX(self.view.frame)-radius,
+                                  CGRectGetMidY(self.view.frame)-radius);
+    
+    // Configure the apperence of the circle
+    circle.fillColor = [UIColor clearColor].CGColor;
+    circle.strokeColor = [UIColor blackColor].CGColor;
+    circle.lineWidth = 5;
+    
+    // Add to parent layer
+    [self.view.layer addSublayer:circle];
+}
+
+-(void)createTheCompassPointer
+{
+
+    CALayer *sublayer = [CALayer layer];
+    sublayer.backgroundColor = [UIColor grayColor].CGColor;
+    sublayer.shadowOffset = CGSizeMake(0, 3);
+    sublayer.frame = CGRectMake(self.view.center.x-14, self.view.center.y-96, 30, 192);
+    sublayer.borderColor = [UIColor blackColor].CGColor;
+    sublayer.borderWidth = 2.0;
+    sublayer.cornerRadius = 10.0;
+    [self.view.layer addSublayer:sublayer];
+    
+}
+
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
