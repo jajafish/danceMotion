433948960dbcc6507b93dbba0e04250c7b80c4c7
Jared Fishman <jfish@gigbear.com>
2014-07-27 06:37:15 -0700

    imageView loaded, most code cut out, but animation not happening

--- outsideMotion/JFDanceCompassVC.m
+++ outsideMotion/JFDanceCompassVC.m
@@ -41,10 +41,7 @@ typedef enum stages {west, east}Direction;
     [self.hotStageSegment insertSegmentWithTitle:@"hip-hop" atIndex:1 animated:YES];
     [self.hotStageSegment addTarget:self action:@selector(hotStageIs:) forControlEvents:UIControlEventValueChanged];
     
-//    [self createTheCentralCircle];
-    
-    
-//    [self createDirectionTriangle];
+
     
     
     self.compassImageView = [[UIImageView alloc]initWithFrame:CGRectMake(40, 140, 240, 240)];
@@ -57,11 +54,7 @@ typedef enum stages {west, east}Direction;
     NSLog(@"the frame of the compass image view is %@", NSStringFromCGRect(frameOfCompassImageView));
     
     
-    [NSTimer scheduledTimerWithTimeInterval:2.5 target:self selector:@selector(guideUserToStageWithCompassAnimation) userInfo:nil repeats:YES];
-    
-//    [self performSelector:@selector(rotateDirectionTriangle) withObject:nil afterDelay:3];
-    
-    // Do any additional setup after loading the view.
+
 }
 
 - (IBAction)hotStageIs:(UISegmentedControl *)sender {
@@ -82,75 +75,24 @@ typedef enum stages {west, east}Direction;
 }
 
 
--(void)createTheCentralCircle
-{
-    int radius = 100;
-    CAShapeLayer *circle = [CAShapeLayer layer];
-    // Make a circular shape
-    circle.path = [UIBezierPath bezierPathWithRoundedRect:CGRectMake(0, 0, 2.0*radius, 2.0*radius)
-                                             cornerRadius:radius].CGPath;
-    // Center the shape in self.view
-    circle.position = CGPointMake(CGRectGetMidX(self.view.frame)-radius,
-                                  CGRectGetMidY(self.view.frame)-radius);
+- (IBAction)rotateTheImage:(UIButton *)sender {
+//    CABasicAnimation *anim = [CABasicAnimation animationWithKeyPath:@"transform"];
+//    anim.fromValue = [NSValue valueWithCATransform3D:self.compassImageView.layer.transform];
+//    anim.toValue = [NSValue valueWithCATransform3D:CATransform3DMakeRotation(M_PI, 0.0, 1.0, 0.0)];
+//    anim.duration = 1.0;
+//    anim.repeatCount = CGFLOAT_MAX;
+//    anim.autoreverses = YES;
+//    anim.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut];
+//    [self.compassImageView.layer addAnimation:anim forKey:@"xform"];
+//    [self.view setNeedsDisplay];
     
-    // Configure the apperence of the circle
-    circle.fillColor = [UIColor clearColor].CGColor;
-    circle.strokeColor = [UIColor blackColor].CGColor;
-    circle.lineWidth = 5;
+    NSLog(@"the compass SHOULD move");
     
-    // Add to parent layer
-    [self.view.layer addSublayer:circle];
-}
-
-
-
--(void)createDirectionTriangle
-{
-
-    CGMutablePathRef path = CGPathCreateMutable();
-    CGPathMoveToPoint(path, NULL, -10, 175);
-    CGPathAddLineToPoint(path, NULL, 190, 75);
-    CGPathAddLineToPoint(path, NULL, 190, 275);
-    CGPathAddLineToPoint(path, NULL, -10, 175);
-    
-    CAShapeLayer *shapeLayer = [[CAShapeLayer alloc]init];
-    [shapeLayer setBounds:CGRectMake(0, 0, 50, 200)];
-    [shapeLayer setFillColor:[[UIColor blackColor]CGColor]];
-    [shapeLayer setPosition:CGPointMake(200, 200)];
-    [shapeLayer setPath:path];
-    
-    self.triangleShape = shapeLayer;
-    
-    [[[self view]layer]addSublayer:self.triangleShape];
-    
-}
-
-
--(void)guideUserToStageWithCompassAnimation
-{
-    
-    CABasicAnimation *rotate = [CABasicAnimation animationWithKeyPath:@"transform"];
-    rotate.fromValue = [NSValue valueWithCATransform3D:CATransform3DIdentity];
-    rotate.toValue = [NSNumber numberWithFloat:((190*M_PI)/ -180)];
-    rotate.duration = 1.0;
-
-    if ([self.hotStage  isEqual: @"Rock"]){
-        NSLog(@"animate the compas for the hot stage ROCK");
-        
-        CATransform3D rotationTransform = CATransform3DMakeRotation(M_PI, 0, 0, 1);
-//        CABasicAnimation *rotationAnimation = [CABasicAnimation animationWithKeyPath:@"transform"];
-        rotate.toValue = [NSValue valueWithCATransform3D:rotationTransform];
-        rotate.duration = 1.0f;
-        rotate.cumulative = YES;
-        rotate.repeatCount = HUGE_VALF;
-    
-        
-    } else if ([self.hotStage  isEqual: @"Hip-hop"]) {
-        NSLog(@"animate the compass for the hot stage of Hip-HOP");
-
-    }
-    
-    [self.triangleShape addAnimation:rotate forKey:@"rotation"];
+    CALayer *layer = self.compassImageView.layer;
+    CABasicAnimation *anim = [CABasicAnimation animationWithKeyPath:@"position"];
+    anim.toValue = [NSValue valueWithCGPoint:CGPointMake(layer.position.x, 0.0)];
+    anim.duration = 2.0;
+    [layer addAnimation:anim forKey:@"anim"];
 
     
 }
@@ -158,17 +100,6 @@ typedef enum stages {west, east}Direction;
 
 
 
--(void)pointCompassWest
-{
-    
-    
-}
-
-
--(void)pointCompassEast
-{
-    
-}
 
 
 
@@ -177,20 +108,7 @@ typedef enum stages {west, east}Direction;
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-//- (IBAction)moveButtonPressed:(id)sender {
-//    
-//    [self rotateDirectionTriangle];
-//}
 
-/*
-#pragma mark - Navigation
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
-{
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
