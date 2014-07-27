eae59d5a5e268f9d5546f30ac31538bc44933471
Jared Fishman <jfish@gigbear.com>
2014-07-27 08:33:24 -0700

    logging kompas data

--- outsideMotion/JFDanceCompassVC.m
+++ outsideMotion/JFDanceCompassVC.m
@@ -7,20 +7,22 @@
 //
 
 #import "JFDanceCompassVC.h"
+#import <CoreLocation/CoreLocation.h>
+
 
 typedef enum stages {west, east}Direction;
 
-@interface JFDanceCompassVC ()
+@interface JFDanceCompassVC () <CLLocationManagerDelegate>
 
+// UI Elements
 @property (weak, nonatomic) IBOutlet UIView *subView;
-
 @property (strong, nonatomic) IBOutlet UIView *danceCompassLogo;
-
 @property (strong, nonatomic) UIImageView *backgroundSafariBrand;
-
 @property (strong, nonatomic) IBOutlet UISegmentedControl *hotStageSegment;
-@property (strong, nonatomic) CALayer *triangleShape;
 @property (strong, nonatomic) NSString *hotStage;
+
+// Compass properties
+@property (strong, nonatomic) CLLocationManager *locationManager;
 
 @end
 
@@ -39,13 +41,14 @@ typedef enum stages {west, east}Direction;
 {
     [super viewDidLoad];
     
-    
+    // set up the background view
     UIImage *backgroundView = [UIImage imageNamed:@"dcbg.png"];
     self.backgroundSafariBrand = [[UIImageView alloc]initWithFrame:CGRectMake(0, 0, 320, 517)];
     self.backgroundSafariBrand.image = backgroundView;
     [self.view insertSubview:self.backgroundSafariBrand belowSubview:self.subView];
-                                
     
+    
+    // set up the segmenet control for genres
     self.hotStageSegment = [[UISegmentedControl alloc]init];
     [self.view addSubview:self.hotStageSegment];
     self.hotStageSegment.frame = CGRectMake(5, 450, 100, 40);
@@ -53,16 +56,34 @@ typedef enum stages {west, east}Direction;
     [self.hotStageSegment insertSegmentWithTitle:@"hip-hop" atIndex:1 animated:YES];
     [self.hotStageSegment addTarget:self action:@selector(hotStageIs:) forControlEvents:UIControlEventValueChanged];
     
-
     
+    // set up the spinning wheel
     _subView.layer.contents = (id)[UIImage imageNamed:@"transTri.png"].CGImage;
     [_subView.layer setOpaque:NO];
     _subView.opaque = NO;
+    
+    
+    // set up the location manager
+    self.locationManager = [[CLLocationManager alloc]init];
+    self.locationManager.delegate = self;
+    [self.locationManager startUpdatingHeading];
+    [self.locationManager startUpdatingLocation];
 
 
+}
+
+-(void)locationManager:(CLLocationManager *)manager didUpdateHeading:(CLHeading *)newHeading
+{
+    
+    NSLog(@"COMPASS DATA");
+    NSLog(@"%@", [NSString stringWithFormat:@"%f", newHeading.magneticHeading]);
+    NSLog(@"%@", [NSString stringWithFormat:@"%f", newHeading.trueHeading]);
+    NSLog(@"%@", [NSString stringWithFormat:@"%f", self.locationManager.location.coordinate.longitude]);
+    NSLog(@"%@", [NSString stringWithFormat:@"%f", self.locationManager.location.coordinate.latitude]);
     
     
 }
+
 
 - (IBAction)hotStageIs:(UISegmentedControl *)sender {
     
@@ -114,12 +135,7 @@ typedef enum stages {west, east}Direction;
     
     [layer addAnimation:anim forKey:@"xform"];
     
-    
-    
 }
-
-
-
 
 
 - (void)didReceiveMemoryWarning
